f89e36be62e1b964dd7bb043817b83cedbfabbbb
Matheus Pires de Sousa <matheuspdsousa@gmail.com>
2023-01-25 23:53:38 -0300

    fix: remove style inline unused

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -3,17 +3,13 @@ import {BottomSheetFlatList, BottomSheetModal} from '@gorhom/bottom-sheet';
 import React, {forwardRef, useMemo} from 'react';
 import {Text, View} from 'react-native';
 
+import * as S from './styles';
+
 export const BottomSheetGallery = forwardRef<BottomSheetModal>((_, ref) => {
   const snapPoints = useMemo(() => ['50%', '75%', '100'], []);
 
   return (
-    <BottomSheetModal
-      ref={ref}
-      index={0}
-      snapPoints={snapPoints}
-      style={{
-        backgroundColor: 'red',
-      }}>
+    <BottomSheetModal ref={ref} index={0} snapPoints={snapPoints}>
       <BottomSheetFlatList
         data={[0, 1]}
         renderItem={() => (
